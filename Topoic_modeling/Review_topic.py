141b92107e5c895dae6aec0b1218f30e23bff72e
SungaHwang <sungahwang@yonsei.ac.kr>
2023-12-05 13:04:56 +0900

    Add topoc modeling

---
+++ Topoic_modeling/Review_topic.py
@@ -0,0 +1,57 @@
+from gensim.corpora import Dictionary
+# import libraries
+import pandas as pd
+import numpy as np
+import pickle
+import os
+import re
+from konlpy.tag import Okt
+okt = Okt()
+from konlpy.tag import Kkma
+kkma = Kkma()
+from collections import Counter
+
+# load dataset
+review = pd.read_csv('.\\reviewdataset\\review_raw.csv')
+review[:3] # check
+
+# drop na
+review = review[(review['직무']!='\n          전직원\n      ')&(review['직무']!='\n          현직원\n      ')&(review['직무'].notna())]
+len(review)
+
+# 장점
+review['장점'] = review['장점'].str.replace("\n"," ")
+review['장점'] = review['장점'].str.replace("\r"," ")
+review['장점'] = review['장점'].str.replace("\t"," ")
+review['장점'] = review['장점'].str.replace(",","")
+
+# 단점
+review['단점'] = review['단점'].str.replace("\n"," ")
+review['단점'] = review['단점'].str.replace("\r"," ")
+review['단점'] = review['단점'].str.replace("\t"," ")
+review['단점'] = review['단점'].str.replace(",","")
+
+# 요약
+review['요약'] = review['요약'].str.replace("\n"," ")
+review['요약'] = review['요약'].str.replace("\r"," ")
+review['요약'] = review['요약'].str.replace("\t"," ")
+review['요약'] = review['요약'].str.replace(",","")
+
+# regular expression
+review['장점']= review['장점'].str.replace(pat=r'[^\w]',repl=r' ',regex=True)
+review['단점']= review['단점'].str.replace(pat=r'[^\w]',repl=r' ',regex=True)
+
+# new 장단점 column by combining 장점 and 단점 col
+review['장단점'] = review['장점']+review['단점']
+
+# extract theme keywords (noun) by 장단점 data
+words_noun = []
+
+for rvw in review['장단점']:
+    pos = okt.nouns(rvw) # only Nouns
+    words_noun.extend(pos)
+# print(words_noun)
+
+# # of elements
+count_noun = Counter(words_noun)
+print(count_noun)
